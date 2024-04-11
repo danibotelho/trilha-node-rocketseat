ccc679de6ddc12bd2cda0737f8c9b523bc7e6c66
danibotelho <danibl26@gmail.com>
2024-04-11 15:04:22 -0300

    configuracao inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+
+
+app.get("/", () => {
+  return "Hello world!";
+});
+
+app.get("/teste", () => {
+  return "Hello Teste!";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running");
+});
